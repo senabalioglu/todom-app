57459f1c590f196364079fcf10854da3ca2e0f7b
senabalioglu <senabalioglu372@gmail.com>
2025-06-28 15:17:27 +0300

    ToDoCard
    ToDoCard oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import ToDoCard from "./components/ToDoCard/ToDoCard";
 
 function App (){
   return (
     <View>
       <Text>App</Text>
+      <ToDoCard />
     </View>
   );
 };
